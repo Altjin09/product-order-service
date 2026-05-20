fb799d272a5fc65bbe90d4efabe7518fc379f6a2
Altjin09 <p.altjin09@gmail.com>
2026-05-20 09:27:07 +0800

    fix: httpclient4 timeout for SOAP client

--- src/main/java/com/ecommerce/store/config/SoapClientConfig.java
+++ src/main/java/com/ecommerce/store/config/SoapClientConfig.java
@@ -1,10 +1,13 @@
 package com.ecommerce.store.config;
 
 import com.ecommerce.store.middleware.SoapAuthClient;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.transport.http.HttpComponentsMessageSender;
 
 @Configuration
 public class SoapClientConfig {
@@ -14,8 +17,20 @@ public class SoapClientConfig {
 
     @Bean
     public SoapAuthClient soapAuthClient() {
+        RequestConfig config = RequestConfig.custom()
+            .setConnectTimeout(10000)
+            .setSocketTimeout(10000)
+            .build();
+
+        HttpClient httpClient = HttpClients.custom()
+            .setDefaultRequestConfig(config)
+            .build();
+
+        HttpComponentsMessageSender sender = new HttpComponentsMessageSender(httpClient);
+
         SoapAuthClient client = new SoapAuthClient();
         client.setDefaultUri(soapAuthUrl);
+        client.setMessageSender(sender);
         return client;
     }
 }
